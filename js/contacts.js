a5f34bfdd78ccf3289a7ea304e8d76b8ef092caa
Redouane Amrani <amrani.redouane@gmail.com>
2019-10-11 01:53:39 +0200

    second  commit

--- js/contacts.js
+++ js/contacts.js
@@ -7,52 +7,54 @@ Activité : gestion des contacts
 console.log("Début du programme\nBienvenue dans le gestionnaire de contacts !\nChoisissez une option:\n1: Lister les contacts\n2: Ajouter un contact\n3: Quitter");
 
 
-let eleves = [{ prenom: 'lola', nom: 'toto' }, { prenom: 'momo', nom: 'fifi' }, { prenom: 'oko', nom: 'gogo' }];
-const options = prompt("Choisissez une option:\n1: Lister les contacts\n2: Ajouter un contact\n0: Quitter ");
+let eleves = [{ prenom: 'Carole', nom: 'Lévisse' }, { prenom: 'Mélodie', nom: 'Nelsonne' }];
 let lastContact = eleves[eleves.length - 1];
 
-
-
-
+  
 function choixOptions() {
-console.log('Option choisie :', options);
+  
+  while (true) {
+  
+    let options = prompt("Choisissez une option:\n1: Lister les contacts\n2: Ajouter un contact\n0: Quitter ");
 
+    console.log('Option choisie :', options);
 
-    if (options == !1 || !2 || !0) {
-        console.log('Recommencer ! Il faut choisir parmis ces options:\n1: Lister les contacts\n2: Ajouter un contact\n0: Quitter ');
-
+    if (options == 0) {
+      console.log("Fin du programme !");
+      window.close();
+      return null;
     }
 
+    // Si utilisateur entre un mauvais choix qui n'est pas parmis les options
+    if (options !=1 && options !=2 && options !=0) {
+        console.log('Recommencer ! Il faut choisir parmis ces options:\n1: Lister les contacts\n2: Ajouter un contact\n0: Quitter ');
+    }
+    // si utilisateur choisi de voir la liste des contacts
     if (options == 1) {
-
         console.log('1- liste de contacts :\n', eleves);
     }
 
-
-
+    // si utilisateur prefere ajouter un contact
     if (options == 2) {
         const prenom = prompt("Entrer un prenom ");
         const nom = prompt("Entrer un nom ");
-        // pusher valeurs dans le tableau eleve
+        // pusher valeurs dans le tableau 
         eleves.push({ prenom, nom });
 
         console.log(`Le nombre de contacts est de : ${eleves.length}`);
         console.log('Dernier contact ajouté :', eleves[eleves.length - 1]);
         console.log(Object.values(eleves));
 
-
     }
-    //    proposer à l'utilisateur de pouvoir continuer pour ajouter un contact ou quitter  ????????
-    
-    
+
+  }
+// fin de la de la boucle WHILE
+
 }
-// fin du programme
-                if (options == 0) {
-                    
-                    // console.log("Fin du programme !");
-                    window.close();
-                }
+// fin de la fonction choixOptions();
+
 choixOptions();
+
 
 
 
